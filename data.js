264e6ec6b21c4d4688999d9a4e6a740791668690
femin1024 <79847316+femin1024@users.noreply.github.com>
2021-03-01 23:01:09 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ var datalist = [
         "time": "월 수  10:30 ~ 11:45",
         "zoom": "id: 898 1942 4524<br>pw: 549746<br><a href='https://kaist.zoom.us/j/89819424524?pwd=bUNzaDQ1OFR3aVd4SzBGMnNkNFVCZz09'>바로가기</a>",
         "prof": "심흥선 @kaist.ac.kr",
-        "ta": "심흥선 @kaist.ac.kr",
+        "ta": "@kaist.ac.kr",
         "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483434'>공지사항</a>"
     },
     {
         "classname": "고체 물리학",
         "time": "화 목  10:30 ~ 11:45",
-        "zoom": "id: <br>pw: <br><a href='https://kaist.zoom.us/j/84688519951?pwd=TjMrOStTMkcvaTN3OXZPWnJZenhmUT09'>바로가기</a>",
-        "prof": "양용수 @kaist.ac.kr",
+        "zoom": "id: 846 8851 9951<br>pw: Unkown<br><a href='https://kaist.zoom.us/j/84688519951?pwd=TjMrOStTMkcvaTN3OXZPWnJZenhmUT09'>바로가기</a>",
+        "prof": "양용수 yongsoo.yang@kaist.ac.kr",
         "ta": " @kaist.ac.kr",
         "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483374'>공지사항</a>"
     },
@@ -20,23 +20,23 @@ var datalist = [
         "time": "월 수  1:00 ~ 2:15",
         "zoom": "id: 963 104 3909 <br><a href ='https://kaist.zoom.us/j/9631043909'>바로가기</a>",
         "prof": "정윤철 @kaist.ac.kr",
-        "ta": "심흥선 @kaist.ac.kr",
+        "ta": "김병곤 qudrhs3414@kaist.ac.kr",
         "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=492380'>공지사항</a>"
     },
     {
         "classname": "머신 러닝",
         "time": "화 목  1:00 ~ 2:15",
         "zoom": "id: <br>pw: <br>link: ",
-        "prof": "문재균 @kaist.ac.kr",
-        "ta": "심흥선 @kaist.ac.kr",
+        "prof": "문재균",
+        "ta": "@kaist.ac.kr",
         "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=492410'>공지사항</a>"
     },
     {
         "classname": "입자 물리학",
         "time": "화 목  2:30 ~ 3:45",
         "zoom": "id: 853 3155 7789<br>pw: 218405<br><a href = 'https://kaist.zoom.us/j/85331557789?pwd=THFFbHdkWUc3MnhkV1IxRnEzRVcrZz09'>바로가기</a> ",
-        "prof": "유종희 @kaist.ac.kr",
-        "ta": "심흥선 @kaist.ac.kr",
+        "prof": "유종희 yoo.jonghee@kaist.ac.kr",
+        "ta": "@kaist.ac.kr",
         "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483383'>공지사항</a>"
     }
 ];
