237d8df4088e8fe75a0a907145c2f891cfcaa0ec
femin1024 <79847316+femin1024@users.noreply.github.com>
2021-08-29 02:12:49 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,49 +2,49 @@ var datalist = [
     {
         "classname": "DSP",
         "time": "월 수  9:00 ~ 10:15",
-        "zoom": "id: 898 1942 4524<br>pw: 549746<br><a href='https://kaist.zoom.us/j/89819424524?pwd=bUNzaDQ1OFR3aVd4SzBGMnNkNFVCZz09'>바로가기</a>",
-        "prof": "심흥선 @kaist.ac.kr",
+        "zoom": "id: <br>pw: <br><a href='https://kaist.zoom.us/'>바로가기</a>",
+        "prof": "김창익 @kaist.ac.kr",
         "ta": "@kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483434'>공지사항</a>"
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=576704'>공지사항</a>"
     },
     {
         "classname": "고체 물리학",
         "time": "월 수  10:30 ~ 11:45",
-        "zoom": "id: 846 8851 9951<br>pw: Unkown<br><a href='https://kaist.zoom.us/j/84688519951?pwd=TjMrOStTMkcvaTN3OXZPWnJZenhmUT09'>바로가기</a>",
+        "zoom": "<a href='https://kaist.zoom.us/j/89303936420?pwd=dldzZWVKZENIWVF5T0JCajdLUGxQZz09'>바로가기</a>",
         "prof": "양용수 yongsoo.yang@kaist.ac.kr",
         "ta": " @kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483374'>공지사항</a>"
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=568034'>공지사항</a>"
     },
     {
         "classname": "양자 정보",
         "time": "화 목  9:00 ~ 10:15",
-        "zoom": "id: 898 1942 4524<br>pw: 549746<br><a href='https://kaist.zoom.us/j/89819424524?pwd=bUNzaDQ1OFR3aVd4SzBGMnNkNFVCZz09'>바로가기</a>",
+        "zoom": "id: 895 7241 9720<br>pw: 975289<br><a href='https://kaist.zoom.us/j/89572419720?pwd=bXBRUmFDQ3pQTDZUT0FEV0hVaWJhZz09'>바로가기</a>",
         "prof": "심흥선 @kaist.ac.kr",
         "ta": "@kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483434'>공지사항</a>"
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=568037'>공지사항</a>"
     },
     {
         "classname": "확기확",
         "time": "화 목  10:30 ~ 11:45",
-        "zoom": "id: 963 104 3909 <br><a href ='https://kaist.zoom.us/j/9631043909'>바로가기</a>",
-        "prof": "정윤철 @kaist.ac.kr",
-        "ta": "김병곤 qudrhs3414@kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=492380'>공지사항</a>"
+        "zoom": "id: <br><a href ='https://kaist.zoom.us/'>바로가기</a>",
+        "prof": "성영철 @kaist.ac.kr",
+        "ta": "4@kaist.ac.kr",
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=576620'>공지사항</a>"
     },
     {
         "classname": "건캠생",
         "time": "화  1:00 ~ 2:45",
-        "zoom": "id: 5884186216<br>pw: n1617<br><a href='https://kaist.zoom.us/j/5884186216?pwd=eTR6aHhnYWdXMkFNR2h1bjcvSFNYQT09'>바로가기</a><br><a href='https://zoom.us/j/4705903587?pwd=WGRRbTZQcHJjZTJQZXV0VG9ldUgzQT09'>연습반</a>",
-        "prof": "문재균",
+        "zoom": "id: <br>pw: <br><a href='https://kaist.zoom.us/'>바로가기</a><br><a href='https://zoom.us/j/4705903587?pwd=WGRRbTZQcHJjZTJQZXV0VG9ldUgzQT09'>연습반</a>",
+        "prof": "김석희",
         "ta": "@kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=492410'>공지사항</a>"
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=578153'>공지사항</a>"
     },
     {
         "classname": "논리적 글쓰기",
         "time": "화 목  4:00 ~ 5:15",
-        "zoom": "id: 853 3155 7789<br>pw: 218405<br><a href = 'https://kaist.zoom.us/j/85331557789?pwd=THFFbHdkWUc3MnhkV1IxRnEzRVcrZz09'>바로가기</a> ",
-        "prof": "유종희 yoo.jonghee@kaist.ac.kr",
+        "zoom": "id: 514 926 6670<br><a href = 'https://kaist.zoom.us/j/5149266670'>바로가기</a> ",
+        "prof": "권미란",
         "ta": "@kaist.ac.kr",
-        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=483383'>공지사항</a>"
+        "note": "<a href='https://klms.kaist.ac.kr/mod/courseboard/view.php?id=578006'>공지사항</a>"
     }
 ];
